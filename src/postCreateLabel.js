484de7674efe340ffe1485beee8f138c564b965b
Mohit Khare <mohitfeuhrer@gmail.com>
2018-12-22 17:29:00 +0530

    Adding basic setup for pull request and releases

---
+++ src/postCreateLabel.js
@@ -0,0 +1,11 @@
+module.exports = (context, {owner, repo, name, color, description}) => {
+    console.log('In postCreateLabel.js..')
+    context.github.issues.createLabel({
+      owner,
+      repo,
+      name: name,
+      color: color,
+      description: description
+    })
+  }
+  
